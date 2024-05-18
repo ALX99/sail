d66ebc1094c6bc1c2f9a65ddd30d486ee401305f
ALX99 <46844683+ALX99@users.noreply.github.com>
2024-05-18 10:26:47 +0900

    feat: implement new minimal UI

--- cmd/fly/fly.go
+++ cmd/fly/fly.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"github.com/alx99/fly/internal/config"
-	"github.com/alx99/fly/internal/state"
 	"github.com/alx99/fly/internal/util"
-	"github.com/alx99/fly/internal/views/primary"
+	"github.com/alx99/fly/internal/views/model"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/rs/zerolog/log"
 )
@@ -29,12 +29,16 @@ func main() {
 		log.Fatal().Err(err).Send()
 	}
 
-	m, err := primary.New(state.NewState(), primary.Config{PWDFile: *pwdFile}, cfg)
-	if err != nil {
-		log.Fatal().Err(err).Send()
-	}
+	// m, err := primary.New(state.NewState(), primary.Config{PWDFile: *pwdFile}, cfg)
+	// if err != nil {
+	// 	log.Fatal().Err(err).Send()
+	// }
 
-	_, err = tea.NewProgram(m, tea.WithAltScreen()).Run()
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	_, err = tea.NewProgram(model.New(cwd, cfg)).Run()
 	if err != nil {
 		log.Fatal().Err(err).Send()
 	}
---
+++ internal/views/model/model.go
@@ -0,0 +1,166 @@
+package model
+
+import (
+	"io/fs"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/alx99/fly/internal/config"
+	"github.com/alx99/fly/internal/util"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/rs/zerolog/log"
+)
+
+type dirLoaded struct {
+	path  string
+	files []fs.DirEntry
+}
+
+type position struct {
+	c, r int
+}
+
+type Model struct {
+	cfg config.Config
+
+	cwd     string        // current working directory
+	files   []fs.DirEntry // current files in that directory
+	maxRows int
+
+	cursor    position            // cursor
+	positions map[string]position // previous positions
+
+	// for performance purposes
+	sb strings.Builder
+}
+
+func New(cwd string, cfg config.Config) Model {
+	return Model{
+		cwd:       cwd,
+		cfg:       cfg,
+		maxRows:   10,
+		positions: make(map[string]position, 100),
+		sb:        strings.Builder{},
+	}
+}
+
+func (m Model) Init() tea.Cmd {
+	return m.loadDir(m.cwd)
+}
+
+func (m Model) logCursor() {
+	log.Debug().Msgf("cursor.r: %v, cursor.c: %v, maxRows: %v", m.cursor.r, m.cursor.c, m.maxRows)
+}
+
+func (m Model) cursorOffset() int {
+	m.logCursor()
+	return m.cursor.c*m.maxRows + m.cursor.r
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		case m.cfg.Settings.Keybinds.NavUp:
+			m.cursor.r = max(0, m.cursor.r-1)
+		case m.cfg.Settings.Keybinds.NavDown:
+			if m.cursor.c < len(m.files)/m.maxRows {
+				m.cursor.r = min(m.cursor.r+1, min(len(m.files), m.maxRows)-1)
+			} else if m.cursor.c == len(m.files)/m.maxRows {
+				m.cursor.r = min(m.cursor.r+1, len(m.files)%m.maxRows-1)
+			}
+			return m, nil
+
+		case m.cfg.Settings.Keybinds.NavLeft:
+			m.cursor.c = max(0, m.cursor.c-1)
+			return m, nil
+		case m.cfg.Settings.Keybinds.NavRight:
+			m.cursor.c++
+			if m.cursorOffset() >= len(m.files) {
+				m.cursor.c-- // undo the cursor move
+			}
+			return m, nil
+		case ",":
+			return m, m.loadDir(path.Dir(m.cwd))
+		case ".":
+			if m.files != nil && m.files[m.cursorOffset()].IsDir() {
+				return m, m.loadDir(path.Join(m.cwd, m.files[m.cursorOffset()].Name()))
+			}
+
+		}
+	case dirLoaded:
+		m.positions[m.cwd] = m.cursor
+
+		m.files = msg.files
+		m.cwd = msg.path
+
+		if cursor, ok := m.positions[m.cwd]; ok {
+			m.cursor = cursor
+
+			// sanity check in case the files has decreased
+			if m.cursorOffset() >= len(m.files) {
+				m.cursor.r = 0
+				m.cursor.c = 0
+			}
+		} else {
+			m.cursor.r = 0
+			m.cursor.c = 0
+		}
+	case error:
+		log.Error().Err(msg).Msg("Error occurred")
+	}
+
+	return m, nil
+}
+
+func (m Model) View() string {
+	m.sb.Reset()
+	m.sb.WriteString(m.cwd + "\n\n")
+
+	grid := make([][]fs.DirEntry, 0, m.maxRows)
+	maxColNameLen := make([]int, len(m.files)/m.maxRows+1)
+
+	for i, f := range m.files {
+		if i < m.maxRows {
+			grid = append(grid, make([]fs.DirEntry, 0, 10))
+		}
+		r, c := i%m.maxRows, i/m.maxRows
+		maxColNameLen[c] = max(maxColNameLen[c], len(f.Name()))
+
+		grid[r] = append(grid[r], f)
+		maxColNameLen[len(grid[r])-1] = max(maxColNameLen[len(grid[r])-1], len(f.Name()))
+	}
+
+	for row := range len(grid) {
+		for col, f := range grid[row] {
+			if m.cursor.r == row && m.cursor.c == col {
+				m.sb.WriteString(">")
+			}
+			extraPadding := maxColNameLen[col] - len(f.Name()) + 2
+
+			if m.cursor.r == row && m.cursor.c == col {
+				extraPadding--
+			}
+			m.sb.WriteString(util.GetStyle(f).
+				PaddingRight(extraPadding).
+				Render(f.Name()))
+		}
+		m.sb.WriteString("\n")
+	}
+
+	return m.sb.String()
+}
+
+func (m Model) loadDir(path string) tea.Cmd {
+	return func() tea.Msg {
+		files, err := os.ReadDir(path)
+		if err != nil {
+			return err
+		}
+		return dirLoaded{files: files, path: path}
+	}
+}
